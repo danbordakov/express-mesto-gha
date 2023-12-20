0e0079dd8a8f240d596fea874e04f5152026e517
danbordakov <danbordakov@yandex.ru>
2023-12-20 14:40:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const userRouter = require("./routes/users");
 const cardRouter = require("./routes/cards");
 const { HTTP_STATUS_NOT_FOUND } = require("http2").constants;
 require("dotenv").config();
+
 const { PORT, DB_PATH } = process.env;
 
 const app = express();
@@ -15,21 +16,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect(DB_PATH).then(console.log("БД запущена"));
 
 app.use((req, res, next) => {
-  req.user = {
-    _id: "6582b7ccf4c10ad3fe6c99a6", // ID захарден
-  };
-  next();
+	req.user = {
+		_id: "6582b7ccf4c10ad3fe6c99a6", // ID захарден
+	};
+	next();
 });
 
 app.use("/", cardRouter);
 app.use("/", userRouter);
 
 app.use((err, res) => {
-  res.status(HTTP_STATUS_NOT_FOUND).send({
-    message: "Данной страницы не существует",
-  });
+	res.status(HTTP_STATUS_NOT_FOUND).send({
+		message: "Данной страницы не существует",
+	});
 });
 
 app.listen(PORT, () => {
-  console.log("Сервер запущен");
+	console.log("Сервер запущен");
 });
