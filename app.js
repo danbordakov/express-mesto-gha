6e9701008361ee2282e176644632194cd1237889
danbordakov <danbordakov@yandex.ru>
2023-12-28 22:55:34 +0300

    ПР14

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const { login, createUser } = require("./controllers/users");
 require("dotenv").config();
 const auth = require("./middlewares/auth");
 
-const { PORT = 3000, DB_PATH = "mongodb://127.0.0.1:27017/mestodb" } =
-  process.env;
+const { PORT = 3000, DB_PATH = "mongodb://127.0.0.1:27017/mestodb" } = process.env;
 
 const app = express();
 
@@ -23,31 +22,31 @@ app.use(cookieParser());
 mongoose.connect(DB_PATH).then(console.log("БД запущена"));
 
 app.post(
-  "/signin",
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-    }),
-  }),
-  login
+	"/signin",
+	celebrate({
+		body: Joi.object().keys({
+			email: Joi.string().required().email(),
+			password: Joi.string().required().min(8),
+		}),
+	}),
+	login,
 );
 
 app.post(
-  "/signup",
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string()
-        .min(2)
-        .max(30)
-        .regex(/^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*/),
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-    }),
-  }),
-  createUser
+	"/signup",
+	celebrate({
+		body: Joi.object().keys({
+			name: Joi.string().min(2).max(30),
+			about: Joi.string().min(2).max(30),
+			avatar: Joi.string()
+				.min(2)
+				.max(30)
+				.regex(/^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*/),
+			email: Joi.string().required().email(),
+			password: Joi.string().required().min(8),
+		}),
+	}),
+	createUser,
 );
 
 app.use(auth);
@@ -56,9 +55,9 @@ app.use("/", cardRouter);
 app.use("/", userRouter);
 
 app.use((err, res) => {
-  res.status(HTTP_STATUS_NOT_FOUND).send({
-    message: "Данной страницы не существует",
-  });
+	res.status(HTTP_STATUS_NOT_FOUND).send({
+		message: "Данной страницы не существует",
+	});
 });
 
 // Обработка ошибок Celebrate
@@ -66,12 +65,12 @@ app.use(errors());
 
 // Централизованная обработка ошибок
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({
-    message: statusCode === 500 ? "На сервере произошла ошибка" : message,
-  });
+	const { statusCode = 500, message } = err;
+	res.status(statusCode).send({
+		message: statusCode === 500 ? "На сервере произошла ошибка" : message,
+	});
 });
 
 app.listen(PORT, () => {
-  console.log("Сервер запущен");
+	console.log("Сервер запущен");
 });
