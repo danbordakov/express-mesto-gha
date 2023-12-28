1f70e6f2f69aac01925dcf048a2b6c6dc543617a
danbordakov <danbordakov@yandex.ru>
2023-12-28 23:26:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use((err, res) => {
 app.use(errors());
 
 // Централизованная обработка ошибок
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
     message: statusCode === 500 ? "На сервере произошла ошибка" : message,
