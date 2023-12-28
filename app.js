0c07e6535877949750cffef9c3a6426b0ff4ffa3
danbordakov <danbordakov@yandex.ru>
2023-12-28 19:57:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,10 @@ app.post(
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().min(2).max(30),
+      avatar: Joi.string()
+        .min(2)
+        .max(30)
+        .regex(/^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*/),
       email: Joi.string().required().email(),
       password: Joi.string().required().min(8),
     }),
